02ecf04918b06bc22f64b7ba67bcbd1a77719c39
vg_varankin <skaldnord@yandex.ru>
2024-04-30 18:18:45 +0300

    update flags with env

--- cmd/shortener/flags.go
+++ cmd/shortener/flags.go
@@ -3,20 +3,18 @@ package main
 import (
 	"errors"
 	"flag"
+	"os"
 	"strings"
 )
 
 var (
 	flagAddress     string
-	flagBaseAddress string
+	flagBaseAddress = "http://localhost:8080"
 )
 
 func ParseFlags() {
-
-	flagBaseAddress = "http://localhost:8080"
-
-	flag.StringVar(&flagAddress, "a", "localhost:8080", "Address:port hosting shortener service")
-	//так как в тесте бывает http нужно распарсить значение
+	flag.StringVar(&flagAddress, "a", ":8080", "Address:port hosting shortener service")
+	//так как в тесте может быть http(возможно s) нужно распарсить значение
 	flag.Func("b", "URL for redirect", func(flagValue string) error {
 		hp := strings.Split(flagValue, ":")
 		if len(hp) < 2 {
@@ -30,6 +28,13 @@ func ParseFlags() {
 
 		return nil
 	})
-
 	flag.Parse()
+
+	if envRunAddr := os.Getenv("SERVER_ADDRESS"); envRunAddr != "" {
+		flagAddress = envRunAddr
+	}
+	if envRunAddr := os.Getenv("BASE_URL"); envRunAddr != "" {
+		flagBaseAddress = envRunAddr
+	}
+
 }
