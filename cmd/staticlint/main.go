ad0866c088de51e9c39d1f94d9279444fa91b85c
vg_varankin <skaldnord@yandex.ru>
2024-08-25 16:06:38 +0300

    analyzer

---
+++ cmd/staticlint/main.go
@@ -0,0 +1,40 @@
+//	Пакет main поддерживает следующие анализаторы
+//
+// exitanalyzer - самописный анализатор os.Exit,
+// errcheck - проверка на обработку ошибок,
+// analysis - стандартный пакет анализаторов.
+package main
+
+import (
+	osexit "github.com/SversusN/shortener/cmd/staticlint/osexit"
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/multichecker"
+	"golang.org/x/tools/go/analysis/passes/printf"
+	"golang.org/x/tools/go/analysis/passes/shadow"
+	"golang.org/x/tools/go/analysis/passes/structtag"
+
+	"github.com/kisielk/errcheck/errcheck"
+	"honnef.co/go/tools/staticcheck" //staticcheck.io
+)
+
+func main() {
+	var chks []*analysis.Analyzer
+
+	// Добаляем все анализаторы staticcheck.
+	for _, v := range staticcheck.Analyzers {
+		chks = append(chks, v.Analyzer)
+	}
+
+	// Дополнительные анализаторы
+	chks = append(
+		chks,
+		osexit.OSExitAnalyzer, // Проверяем os.Exit в main.
+		printf.Analyzer,       // Проверяем формтированную печать printf.
+		shadow.Analyzer,       // Проверяем shadow-переопределения.
+		structtag.Analyzer,    // Проверяем правильность типов структур.
+		errcheck.Analyzer,     // Проверяем обработку ошибок.
+	)
+
+	//Запускаем multichecker
+	multichecker.Main(chks...)
+}
--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/SversusN/shortener
 
-go 1.21.9
+go 1.22.1
+
+toolchain go1.22.2
 
 require (
 	github.com/go-chi/chi/v5 v5.0.12
@@ -8,8 +10,11 @@ require (
 	github.com/golang-migrate/migrate/v4 v4.17.1
 	github.com/google/uuid v1.6.0
 	github.com/jackc/pgx/v5 v5.6.0
+	github.com/kisielk/errcheck v1.7.0
 	github.com/stretchr/testify v1.9.0
 	go.uber.org/zap v1.27.0
+	golang.org/x/tools v0.21.1-0.20240531212143-b6235391adb3
+	honnef.co/go/tools v0.5.1
 )
 
 require (
@@ -25,7 +30,9 @@ require (
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/crypto v0.20.0 // indirect
-	golang.org/x/sync v0.5.0 // indirect
+	golang.org/x/exp/typeparams v0.0.0-20231108232855-2478ac86f678 // indirect
+	golang.org/x/mod v0.17.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
